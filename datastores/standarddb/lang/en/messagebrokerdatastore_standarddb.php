69ab82fc7c4332494abc47573c072bd092aa9526
Cameron Ball <cameronball@catalyst-au.net>
2023-08-23 14:43:52 +0800

    Add language string files

--- datastores/standarddb/lang/en/messagebrokerdatastore_standarddb.php
+++ datastores/standarddb/lang/en/messagebrokerdatastore_standarddb.php
@@ -1,1 +1,16 @@
 <?php
+
+/**
+ * Strings for messagebrokerdatastore_standarddb.
+ *
+ * @package   messagebrokerdatastore_standarddb
+ * @copyright 2023 Monash University
+ * @author    Cameron Ball <cameronball@catalyst-au.net>
+ */
+
+declare(strict_types=1);
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Standard DB';
+$string['privacy:metadata'] = 'The Standard DB Message Broker Datastore plugin does not store any personal data';
--- lang/en/tool_messagebroker.php
+++ lang/en/tool_messagebroker.php
@@ -1,1 +1,16 @@
 <?php
+
+/**
+ * Strings for tool_messagebroker.
+ *
+ * @package   tool_messagebroker
+ * @copyright 2023 Monash University
+ * @author    Cameron Ball <cameronball@catalyst-au.net>
+ */
+
+declare(strict_types=1);
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Message Broker';
+$string['privacy:metadata'] = 'The Message Broker plugin does not store any personal data';
