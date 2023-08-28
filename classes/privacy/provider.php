fe9a0736645d4bed6690dda54ea3862685ba806b
Cameron Ball <cameronball@catalyst-au.net>
2023-08-28 14:44:35 +0800

    Implement null privacy providers for main plugin and standarddb datastore

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -1,1 +1,24 @@
 <?php
+
+declare(strict_types=1);
+
+namespace tool_messagebroker\privacy;
+
+use core_privacy\local\metadata\null_provider;
+
+/**
+ * Privacy provider.
+ *
+ * @package   tool_messagebroker
+ * @copyright 2023 Monash University
+ * @author    Cameron Ball <cameronball@catalyst-au.net>
+ */
+class provider implements null_provider {
+
+    /**
+     * Constructor.
+     */
+    public static function get_reason(): string {
+        return 'privacy:metadata';
+    }
+}
